9cef6ca9c8f3a78a4b51581eb3c831269e79b5a6
KyleNorlander <54556328+KyleNorlander@users.noreply.github.com>
2019-11-18 16:27:51 -0500

    Complete

--- Scenes/FPSCharacter.gd
+++ Scenes/FPSCharacter.gd
@@ -3,13 +3,18 @@ extends KinematicBody
 onready var camera = $Pivot/Camera
 
 var gravity = -30
+var jump_speed = 12
 var max_speed = 8
 var mouse_sensitivity = 0.002 
 
 var velocity = Vector3()
+var jump = false
 
 func get_input():
     var input_dir = Vector3()
+    jump = false
+    if Input.is_action_just_pressed("jump"):
+	    jump = true
     if Input.is_action_pressed("move_forward"):
         input_dir += -camera.global_transform.basis.z
     if Input.is_action_pressed("move_back"):
@@ -25,3 +30,19 @@ func _unhandled_input(event):
     if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
         rotate_y(-event.relative.x * mouse_sensitivity)
         $Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+	   $Pivot.rotation.x = clamp ($Pivot.rotation.x, -1.2,1.2)
+	
+func _physics_process(delta):
+    velocity.y += gravity * delta
+    var desired_velocity = get_input() * max_speed
+
+    velocity.x = desired_velocity.x
+    velocity.z = desired_velocity.z
+    velocity = move_and_slide(velocity, Vector3.UP, true)
+    if jump and is_on_floor():
+	    velocity.y = jump_speed
+		
+	
+		
+		
+
